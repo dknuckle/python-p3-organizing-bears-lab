9a1c419eb8963eb75c6e98e7793462439c2a4348
Dalton Knuckles <djck244@gmail.com>
2023-09-03 09:44:04 -0700

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 4, 'M', 'Brown', 'Nice', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'Black', 'Mean', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'White', 'Nice', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergent Brown', 5, 'M', 'Brown', 'Mean', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'Pink', 'Nice', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'M', 'Green', 'Mean', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'F', 'Black', 'Nice', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, 'F', 'White', 'Nice', 1);
